b4f05bb185d2f8c4e91cefc543b2b5829cf6ada4
ZzAZz4 <esteban.villacorta@utec.edu.pe>
2020-04-20 20:02:57 -0700

    assignment done

--- web/server.py
+++ web/server.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template, request, session, Response, redirect
+
 from database import connector
 from model import entities
 import json
@@ -15,6 +16,16 @@ def static_content(content):
     return render_template(content)
 
 
+@app.route('/palindrome/<word>')
+def palindrome(word):
+    return word + (" is a palindrome!" if word == word[::-1] else " is not a palindrome") 
+
+
+@app.route('/multiplo/<num1>/<num2>')
+def multiplo(num1, num2):
+    if int(num2) == 0:
+        return "ERROR: input invalido para num2: no es posible dividir entre 0"
+    return num1 + ('' if int(num1) % int(num2) == 0 else " no") + " es multiplo de " + num2
 
 
 if __name__ == '__main__':
